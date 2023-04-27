f161a2b3b6c2c3733a2d2995c2973e7c0aeab442
Fabien2S <18100602+Fabien2S@users.noreply.github.com>
2023-04-27 17:33:19 +0200

    Add LogManager

---
+++ src/Igloo.Common/Logging/LogManager.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Igloo.Common.Logging;
+
+public static class LogManager
+{
+    public static ILoggerFactory Factory { get; set; } = new NullLoggerFactory();
+
+    public static ILogger Create(Type type)
+    {
+        return Factory.CreateLogger(type);
+    }
+
+    public static ILogger<T> Create<T>()
+    {
+        return Factory.CreateLogger<T>();
+    }
+}
