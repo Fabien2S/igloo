83dfd513376a82ff35b7f2a05a8992f238759f8d
Fabien2S <18100602+Fabien2S@users.noreply.github.com>
2023-04-30 17:46:06 +0200

    Add Minecraft class

---
+++ src/Igloo.Common/Minecraft.cs
@@ -0,0 +1,11 @@
+﻿namespace Igloo.Common;
+
+public static class Minecraft
+{
+    public const string Namespace = "minecraft";
+
+    public static Identifier Id(ReadOnlySpan<char> path)
+    {
+        return Identifier.Create(Namespace, path);
+    }
+}
